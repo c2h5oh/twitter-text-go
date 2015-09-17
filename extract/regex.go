9e494a2efa435975e267c8bb33ebec546426cd1b
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2015-09-17 16:00:05 -0400

    Simplifies regexes using Unicode Classes

--- extract/regex.go
+++ extract/regex.go
@@ -3,24 +3,13 @@ package extract
 import "regexp"
 
 const (
-	punctuationChars = `!"#\$%&'\(\)\*\+,-\./:;<=>\?@\[\]\^_` + "`" + `\{\|\}~`
+	punctuationChars = `\p{P}$`
 
-	unicodeSpaces = "\u0009-\u000d" + //  # White_Space # Cc   [5] <control-0009>..<control-000D>
-		"\u0020" + // White_Space # Zs       SPACE
-		"\u0085" + // White_Space # Cc       <control-0085>
-		"\u00a0" + // White_Space # Zs       NO-BREAK SPACE
-		"\u1680" + // White_Space # Zs       OGHAM SPACE MARK
-		"\u180E" + // White_Space # Zs       MONGOLIAN VOWEL SEPARATOR
-		"\u2000-\u200a" + // # White_Space # Zs  [11] EN QUAD..HAIR SPACE
-		"\u2028" + // White_Space # Zl       LINE SEPARATOR
-		"\u2029" + // White_Space # Zp       PARAGRAPH SEPARATOR
-		"\u202F" + // White_Space # Zs       NARROW NO-BREAK SPACE
-		"\u205F" + // White_Space # Zs       MEDIUM MATHEMATICAL SPACE
-		"\u3000" // White_Space # Zs       IDEOGRAPHIC SPACE
+	unicodeSpaces = `\p{Cc}\p{Z}`
 
 	unicodeSpacesSet = `[` + unicodeSpaces + `]`
 
-	controlChars = "\x00-\x1F\x7F"
+	controlChars = `\p{Cc}`
 
 	invalidChars = "\uFFFE\uFEFF\uFFFF\u202A\u202B\u202C\u202D\u202E"
 
@@ -31,37 +20,9 @@ const (
 		"\u0300-\u036f" + // Combining diacritics
 		"\u1e00-\u1eff" // Latin Extended Additional (mostly for Vietnamese)
 
-	hashtagAlphaChars = `a-z` + latinAccentChars +
-		"\u0400-\u04ff\u0500-\u0527" + // Cyrillic
-		"\u2de0-\u2dff\ua640-\ua69f" + // Cyrillic Extended A/B
-		"\u0591-\u05bf\u05c1-\u05c2\u05c4-\u05c5\u05c7" +
-		"\u05d0-\u05ea\u05f0-\u05f4" + // Hebrew
-		"\ufb1d-\ufb28\ufb2a-\ufb36\ufb38-\ufb3c\ufb3e\ufb40-\ufb41" +
-		"\ufb43-\ufb44\ufb46-\ufb4f" + // Hebrew Pres. Forms
-		"\u0610-\u061a\u0620-\u065f\u066e-\u06d3\u06d5-\u06dc" +
-		"\u06de-\u06e8\u06ea-\u06ef\u06fa-\u06fc\u06ff" + // Arabic
-		"\u0750-\u077f\u08a0\u08a2-\u08ac\u08e4-\u08fe" + // Arabic Supplement and Extended A
-		"\ufb50-\ufbb1\ufbd3-\ufd3d\ufd50-\ufd8f\ufd92-\ufdc7\ufdf0-\ufdfb" + // Pres. Forms A
-		"\ufe70-\ufe74\ufe76-\ufefc" + // Pres. Forms B
-		"\u200c" + // Zero-Width Non-Joiner
-		"\u0e01-\u0e3a\u0e40-\u0e4e" + // Thai
-		"\u1100-\u11ff\u3130-\u3185\uA960-\uA97F\uAC00-\uD7AF\uD7B0-\uD7FF" + // Hangul (Korean)
-		`\p{Hiragana}` + // Japanese Hiragana
-		"\u30a1-\u30fa\u30fc-\u30fe" + // Katakana (full-width)
-		"\uff66-\uff9f" + // Katakana (half-width)
-		"\uff10-\uff19\uff21-\uff3a\uff41-\uff5a" + // Latin (full-width)
-		"\u3400-\u4dbf" + // Kanji (CJK Extension A)
-		"\u4E00-\u9FFF" + // Kanji (Unified)
-		"\U00020000-\U0002A6DF" + // Kanji (CJK Extension B)
-		"\U0002A700-\U0002B73F" + // Kanji (CJK Extension C)
-		"\U0002B740-\U0002B81F" + // Kanji (CJK Extension D)
-		"\U0002F800-\U0002FA1F" + // Kanji (CJK supplement)
-		"\u3003\u3005\u303b" + // Kanji/Han iteration marks
-		"\uff21-\uff3a\uff41-\uff5a" + // full width Alphabet
-		"\uff66-\uff9f" + // half width Katakana
-		"\uffa1-\uffdc" // half width Hangul (Korean)
+	hashtagAlphaChars = `\p{L}\p{M}` + "\u200c" + "\u3003\u3005\u303b" + "\u0080-\u00ff"
 
-	hashtagAlphaNumericChars = "0-9_\uff10-\uff19" + hashtagAlphaChars
+	hashtagAlphaNumericChars = `\p{N}_` + hashtagAlphaChars
 
 	hashtagAlphaSet        = `[` + hashtagAlphaChars + `]`
 	hashtagAlphaNumericSet = `[` + hashtagAlphaNumericChars + `]`
